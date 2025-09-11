e96d758dc529a5f7d4034f88460099c6d671d130
Rion Asari <rionasari157@gmail.com>
2025-09-11 17:46:05 +0700

    update

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function showMessage() {
-  alert("Halo! Terima kasih sudah mengunjungi portofolio saya 🙌");
+  console.log("Portfolio loaded!");
 }
+
+window.addEventListener("scroll", function () {
+  document.querySelectorAll(".fade-in").forEach(section => {
+    const position = section.getBoundingClientRect().top;
+    if (position < window.innerHeight - 100) {
+      section.classList.add("visible");
+    }
+  });
+});
